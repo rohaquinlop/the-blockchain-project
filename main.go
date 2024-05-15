5791e25e0891461bfbfa90a8ee6038ac25574503
Robin Quintero <rohaquinlop301@gmail.com>
2024-05-15 01:45:48 -0500

    blockchain(go): add proof of work and validate it

--- main.go
+++ main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 BTC to Robin")
+	bc.AddBlock("Send 2 more BTC to Robin")
 
 	for _, block := range bc.blocks {
+		pow := NewProofOfWork(block)
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
